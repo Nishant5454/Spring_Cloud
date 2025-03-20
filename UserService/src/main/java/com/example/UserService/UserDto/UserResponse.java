182acf5c845007f60b42b90263a386d266e1717d
Nishant5454 <bhagatnishant5454@gmail.com>
2025-03-20 22:39:10 +0530

    [Nishant]Add .Dockerfile to each microservice

--- UserService/src/main/java/com/example/UserService/UserDto/UserResponse.java
+++ UserService/src/main/java/com/example/UserService/UserDto/UserResponse.java
@@ -1,8 +1,6 @@
 package com.example.UserService.UserDto;
 
 import com.example.UserService.Entity.Order;
-import com.example.UserService.Entity.User;
-//import jakarta.persistence.criteria.Order;
 import lombok.*;
 
 import java.util.List;
